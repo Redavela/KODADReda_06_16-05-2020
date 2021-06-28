98e572d7febd51d2baa93bb43c420e5c9be39ed1
Redavela <kodadreda67@gmail.com>
2021-06-28 18:25:00 +0200

    key

--- acceuil.js
+++ acceuil.js
@@ -80,18 +80,33 @@ function filterElements (dataJson, tagName) {
 /// ////  Le FILTRE !!!!  //////////
 function addActiveClass (dataJson) {
   const buttons = document.querySelectorAll('.filters_container li')
-  buttons.forEach(btn => btn.addEventListener('click', () => {
-    const active = btn.classList.contains('active')
-    buttons.forEach(btn => btn.classList.remove('active'))
-    if (!active) {
-      btn.classList.add('active')
-    } else {
-      const photographersDiv = document.getElementById('container')
-      photographersDiv.innerHTML = ''
-      filterElements(dataJson, 'all')
+  buttons.forEach(btn =>
+  {
+    btn.addEventListener('click', () => {
+      const active = btn.classList.contains('active')
+      buttons.forEach(btn => btn.classList.remove('active'))
+      if (!active) {
+        btn.classList.add('active')
+      } else {
+        const photographersDiv = document.getElementById('container')
+        photographersDiv.innerHTML = ''
+        filterElements(dataJson, 'all')
+      }
+    })
+    btn.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        const active = btn.classList.contains('active')
+        buttons.forEach(btn => btn.classList.remove('active'))
+        if (!active) {
+          btn.classList.add('active')
+        } else {
+          const photographersDiv = document.getElementById('container')
+          photographersDiv.innerHTML = ''
+          filterElements(dataJson, 'all')
+        }
     }
-  }))
-}
+  })
+  })
 
 function displayPhotographers (dataJson) {
   const buttons = document.querySelectorAll('.filters_container li')
